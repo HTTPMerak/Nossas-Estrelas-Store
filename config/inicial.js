9efab34a1418590e823bb56826ec6492d84515d8
Ricardo Negreiros <75104283+HTTPMerak@users.noreply.github.com>
2022-11-07 21:46:10 -0300

    update

---
+++ config/inicial.js
@@ -0,0 +1,384 @@
+// Configuração inicial - Grand Store Personalizados
+var avisoConfig = ''; // Nao mexer
+var v = 2;
+
+////// Configurações gerais
+var logoDesktop = 200;
+var logoMobile = 38;
+var desejos = true;
+var desejosIcone = 'desejosIcone1';
+var desejosTxt = 'Desejos';
+var desejosAdd = 'Adicionar aos desejos';
+var rastreioRapido = 1;
+var topoFlutuante = true;
+var infoDestaque = true;
+var infoDestaqueIcone = 'iconePix';
+var infoDestaqueTxt = 'Pague com PIX';
+var infoDestaqueTxt2 = 'e ganhe 10% de desconto';
+var infoDestaqueLink = '';
+var marcasSingular = 'Marca';
+var marcasPlural = 'Marcas';
+var marcasCarrosselTxt = 'Escolha pela marca';
+var marcasCarrosselAuto = true;
+var notificacao = true;
+var notificacaoD = 1;
+
+////// Atendimento
+var suporte = 2;
+var suporteLado = 1;
+var suporteAltura = 20;
+var suporteDistancia = 20;
+
+var wpTxt = 'Estamos no whatsapp';
+var wp1 = '(11) 97345-2327';
+var wp1i = 'Fernanda';
+var wp1d = 'Vendas';
+var wp1m = 'Olá, gostaria de tirar uma dúvida. Poderiam me ajudar?';
+var wp1a = 'https://cdn.awsli.com.br/1781/1781687/arquivos/dep4.png'; 
+var wp2 = '(11) 92009-1157';
+var wp2i = 'Luana';
+var wp2d = 'Vendas';
+var wp2m = 'Olá, gostaria de entrar em contato com a loja GrandStore';
+var wp2a = 'https://cdn.awsli.com.br/1781/1781687/arquivos/dep5.png';
+var wp3 = '';
+var wp3i = '';
+var wp3d = '';
+var wp3m = '';
+var wp3a = '';
+var telTxt = 'Compre por telefone';
+var tel1 = '4004-9292';
+var tel1i = '';
+var tel2 = '';
+var tel2i = '';
+var tel3 = '';
+var tel3i = '';
+var mailTxt = 'Envie uma mensagem';
+var mailN = 13;
+var mail1 = 'suporte@groomb.com.br';
+var mail1i = '';
+var mail2 = '';
+var mail2i = '';
+var mail3 = '';
+var mail3i = '';
+var spTxt = 'Vamos conversar?';
+var sp1 = '';
+var sp1i = '';
+var sp2 = '';
+var sp2i = '';
+var sp3 = '';
+var sp3i = '';
+var h1 = 'seg a sex das 10h às 18h';
+var h2 = 'sab das 10h às 14h';
+var h3 = '';
+
+////// Carrinho/checkout
+var carrinhoIcone = 'carrinhoIcone2';
+var ccL = 2;
+var ccE = true;
+var ccS = true;
+var alertaF = true;
+var alertaV = 2;
+var alertaTxt = 'FRETE GRÁTIS';
+var alertaSD = '199.00';
+var alertaSL = '99.00';
+var alertaCO = '299.00';
+var alertaNE = '399.00';
+var alertaNT = '499.00';
+var alerta1 = '';
+var alerta11 = '';
+var alerta1V = '';
+var alerta2 = '';
+var alerta22 = '';
+var alerta2V = '';
+var alerta3 = '';
+var alerta33 = '';
+var alerta3V = '';
+var sCart = true;
+var cCart = true;
+var cCartT = 3;
+var cCartQ = 300;
+var cCartC = 'CUPOM20';
+var cCartB = '';
+var dPx = false;
+var dPN = 'Pague menos';
+var dPxE = 2;
+var dPP = '- Promo';
+var dPT = 1;
+var dPV = 1;
+var dP1q = 5;
+var dP1d = 10;
+var dP1c = 'CUPOM10';
+var dP2q = 400;
+var dP2d = 20;
+var dP2c = 'CUPOM20';
+var dP3q = 600;
+var dP3d = 30;
+var dP3c = 'CUPOM30';
+var agE = true;
+var agEH = 1;
+var agED = 4;
+var agET = 15;
+var agEN = '09/08/2022';
+var ebPx = true;
+var ebPP = '172172528';
+var ebPV = 4;
+
+////// Menu Superior
+var categorias = 5; 
+var menuModelo = 2;
+var menuAbertura = 4;
+
+var produtosMenu = true;
+
+var todasCategorias = true;
+var todasCategoriasTxt = 'Categorias';
+
+var iconesCategorias = true;
+var iconesCategoriasTamanho = 29;
+var iconeCategoria1 = 'https://cdn.awsli.com.br/2473/2473092/arquivos/caneca.png';
+var iconeCategoria2 = 'https://cdn.awsli.com.br/2473/2473092/arquivos/escritorio.png';
+var iconeCategoria3 = 'https://cdn.awsli.com.br/2473/2473092/arquivos/camiseta.png';
+var iconeCategoria4 = 'https://cdn.awsli.com.br/2473/2473092/arquivos/festas.png';
+var iconeCategoria5 = 'https://cdn.awsli.com.br/2473/2473092/arquivos/brindes.png';
+var iconeCategoria6 = '';
+var iconeCategoria7 = '';
+var iconeCategoria8 = '';
+var iconeCategoria9 = '';
+var iconeCategoria10 = '';
+
+var paginaMenu1 = 'Blog';
+var paginaMenu1Link = '/pagina/blog.html';
+var paginaMenu2 = '';
+var paginaMenu2Link = '';
+var paginaMenu3 = '';
+var paginaMenu3Link = '';
+
+////// Ofertas
+var ofertas = true;
+var ofertasTitulo = 'Promoções';
+var iconeOfertas = 'iconeOfertas1';
+var ofertasTxt = 'Somente hoje!!';
+var ofertasSub = 'Aproveite nossas ofertas com 30% de desconto';
+var ofertasListagem = false;
+var ofertasID = '';
+var ofertasTimer = true;
+//var ofertasTimerData = '07/12/2021';
+
+////// Banners
+var fullFundo = '';
+var tarjaAvancado = true;
+var tarja1Icone = 'iconeFrete';
+var tarja1Txt = 'Frete grátis';
+var tarja1Sub = 'Compras acima de R$ 399';
+var tarja1Link = '/pagina/frete-gratis';
+var tarja2Icone = 'iconeCartao';
+var tarja2Txt = 'Parcelamento';
+var tarja2Sub = 'Em até 6x sem juros';
+var tarja2Link = '';
+var tarja3Icone = 'iconeBoleto';
+var tarja3Txt = 'Pagamento à vista';
+var tarja3Sub = '10% de desconto no boleto';
+var tarja3Link = '';
+var tarja4Icone = 'iconeSeguranca';
+var tarja4Txt = 'Segurança';
+var tarja4Sub = 'Loja com SSL de proteção';
+var tarja4Link = '';
+var escolhaTxt = 'Escolha por categoria';
+var escolhaImg = 8;
+
+////// Listagem
+var fixarImagem = false;
+var varL = true;
+var prodTxt = 2;
+var fixarAdicionar = true;
+var adicionar = 'Adicionar';
+var produtosCarrossel = true;
+var produtosCarrosselAuto = false;
+var produtosLinhaDesktop = 5;
+var produtosLinhaMobile = 2;
+var precoC = false;
+var precoP = false;
+var precoA = false;
+var comprarWhatsapp = true;
+var comprarWhatsappT = 'Comprar pelo whatsapp';
+var comprarWhatsappM = 'Olá, pode me ajudar? Eu gostaria de mais informações para a compra do produto:';
+var comprarWhatsappV = false;
+var comprarWhatsappC = false;
+var bP = true;
+var bPCEP = false;
+var bPFrete = false;
+var bPp1 = '';
+var bPt1 = '';
+var bPd1 = '';
+var bPp2 = '';
+var bPt2 = '';
+var bPd2 = '';
+var bPp3 = '';
+var bPt3 = '';
+var bPd3 = '';
+var tituloLancamentos = 'Lançamentos';
+var tituloVendidos = 'Mais vendidos';
+var tituloDestaques = 'Destaques';
+
+////// Video
+var videoDestaque = true;
+var videoTxt = 'Garanta sua camiseta personalizada';
+var videoLink = 'https://www.youtube.com/watch?v=PxcuBytOkQQ';
+var videoProduto = true;
+var videoProdutoTxt = 'Produtos no vídeo';
+var videoProdutoAuto = true;
+
+////// Depoimentos
+var depoimentos = true;
+var depoimentosAuto = false;
+var depoimentosTxt = 'Quem já comprou e recomenda';
+
+var dep1Avatar = 'https://bit.ly/3vdhy0T';
+var dep1Msg = 'O produto chegou perfeitamente. Estava muito bem embalado. Recomendo totalmente a loja.';
+var dep1Nome = 'Pedro Henrique - São Paulo/SP';
+var dep1Link = 'https://google.com.br';
+
+var dep2Avatar = 'https://bit.ly/3vhVbHU';
+var dep2Msg = 'Tudo dentro dos conformes. Com certeza voltarei a comprar mais vezes com certeza.';
+var dep2Nome = 'José Silveira - Canoas/RS';
+var dep2Link = '';
+
+var dep3Avatar = 'https://bit.ly/3vhVbHU';
+var dep3Msg = 'Já é a segunda vez que compro e não me decepiono. Muito obrigado pelo serviço!';
+var dep3Nome = 'Luna Calheiros - Salvador/BA';
+var dep3Link = '';
+
+var dep4Avatar = 'https://bit.ly/3zIH3Kx';
+var dep4Msg = 'No começo eu desconfiei, mas chegou tudo certinho e muito rápido. Voltarei a comprar.';
+var dep4Nome = 'Mirella Souza - Formataleza/CE';
+var dep4Link = '';
+
+var dep5Avatar = 'https://bit.ly/3cwH7Ue';
+var dep5Msg = 'Ameeeeeei! Era bem melhor do que eu imagina, chegou super rápido e bem embalado!';
+var dep5Nome = 'Natalia Mendes - Guarulhos/SP';
+var dep5Link = '';
+
+////// Blog
+var blog = true;
+var blogData = true;
+var blogResumo = true;
+var blogComentarios = true;
+var blogCompartilhar = true;
+var blogFeed = true;
+var blogFeedTxt = 'Últimas postagens';
+var blogAutor = true;
+var autor1Avatar = 'https://cdn.awsli.com.br/1808/1808111/arquivos/author.png';
+var autor1 = 'Luiz Carlos';
+var autor1Bio = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus iaculis, lorem at condimentum molestie, quam lacus auctor metus, at laoreet magna neque et turpis.';
+var autor2Avatar = 'https://cdn.awsli.com.br/1808/1808111/arquivos/author2.png';
+var autor2 = 'Carlos Verissimo';
+var autor2Bio = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus iaculis, lorem at condimentum molestie, quam lacus auctor metus, at laoreet magna neque et turpis.';
+var autor3Avatar = 'https://cdn.awsli.com.br/1808/1808111/arquivos/author3.png';
+var autor3 = 'Luiza Medeiros';
+var autor3Bio = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus iaculis, lorem at condimentum molestie, quam lacus auctor metus, at laoreet magna neque et turpis.';
+
+////// Newsletter
+var cupomNewsletter = 'CUPOM10';
+var imgNewsletter = '';
+
+////// Redes Sociais
+var feedInstagram = true;
+var feedInstagramTxt = 'Gostou? Segue a gente';
+//var feedInstagramToken = '';
+var facebookBox = true;
+
+////// Rodape
+var rodapeLogo = 'https://cdn.awsli.com.br/2473/2473092/arquivos/logo22.png';
+var rodapeEndereco = 'Av. Paulista, 575 - CJ 1903 - Bela Vista, São Paulo SP - CEP: 01311-911';
+
+var rodapeSocial = 'Redes sociais';
+var rodapeSobre = 'Sobre nós';
+var rodapePaginas = 'Institucional';
+var rodapePagamento = 'Formas de pagamento';
+var rodapeSelos = 'Selos de segurança';
+
+var formasEnvio = true;
+var formasEnvioTxt = 'Formas de envio';
+var formaEnvio1 = 'https://bit.ly/3PTe8s2';
+var formaEnvio2 = 'https://bit.ly/3PJv5FG';
+var formaEnvio3 = 'https://bit.ly/3RQtZJQ';
+
+////// Pagina do produto
+var imagemProduto = 550;
+var compartilhamento = true;
+var resumoFlutuante = true;
+var calculadoraFrete = true;
+var codigoTxt = 'Cód';
+var disponibilidadeTxt = '';
+var descricaoTxt = 'Descrição';
+var relacionadosTxt = 'Aproveite e compre também';
+var variacaoCor = true;
+var variacaoTxt = 'Primeiro você deve escolher uma opção.';
+var descricaoCurta = true;
+var pagamentoCompacto = true;
+var pagamentoTxt = 'mais formas de pagamento';
+var avista = 'no pix';
+var avistaDesconto = true;
+var pixPP = true;
+var pixPPValor = 10;
+var pixPPA = true;
+var pixPPB = true;
+var personalizador = true;
+var peB = 'Personalize';
+var peBT = 'Personalizar';
+var peU = 'Observações';
+var pe1T = 1;
+var pe1P = 'Personalize com imagem e texto';
+var pe1Txt = 'Escreva seu texto';
+var pe1F = 'JPG, PNG, GIF';
+var pe1O = 1;
+var pe1L = 62;
+var pe1Obg = true;
+var pe1Aux = '';
+var pe1V1 = '';
+var pe1V2 = '';
+var pe1V3 = '';
+var pe2T = 2;
+var pe2P = 'Personalize com imagem';
+var pe2Txt = 'Envie sua imagem';
+var pe2F = 'jpg, png, gif, psd, crd';
+var pe2O = 3;
+var pe2L = 2;
+var pe2Obg = true;
+var pe2Aux = '';
+var pe2V1 = '';
+var pe2V2 = '';
+var pe2V3 = '';
+var tabelaMedidas = true;
+var tabelaMedidasProduto = true;
+var tabelaNome = 'Tabela de medidas';
+var tabelaImg1 = 'https://bit.ly/3oxMjdu';
+var tabelaPalavra1 = 'Camiseta';
+var tabelaImg2 = '';
+var tabelaPalavra2 = '';
+var tabelaImg3 = '';
+var tabelaPalavra3 = '';
+var validade = false;
+var validadeTxt = 'Validade:';
+var validadeA = 'Perto do vencimento';
+var validadeD = 30;
+var prevenda = false;
+var prevendaTxt = 'Pré-Venda';
+var prevendaTxt2 = 'Lançamento:';
+var prevendaTxt3 = 'O envio é feito em até 48h após o lançamento.';
+var prevendaC = true;
+var calc = false;
+var calcPalavra = 'camisa';
+var calcObj = 'rolos';
+var calcTxt = 'm2';
+var calcATxt = 'Altura da parede';
+var calcA = 1.5;
+var calcLTxt = 'Largura da parede';
+var calcL = 0.53;
+var calcDuvida = '/pagina/como-medir.html';
+var lEx = true;
+
+var cModelo = 2;
+var cFundoEscuro = false;
+var cOfertasFundo = true;
+var csOfertasFundo = true;
